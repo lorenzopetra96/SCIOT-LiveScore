4b195b988fa9af39391d1ca914b29dd2203d5e50
lorenzopetra96 <lorenzopetra@gmail.com>
2021-11-16 17:16:32 +0100

    fix prod

--- producerLiveScore.py
+++ producerLiveScore.py
@@ -15,25 +15,27 @@ channel.exchange_declare(exchange='livescore', exchange_type='direct')
 print("...eseguito")
 
 headers = { 
-  "apikey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
+  "apikey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
 
 paramsAPI = (
-   ("season_id","xxx"),
+   ("season_id","392"),
    ("date_from","2021-09-19"),
    ("live","true")
 )
 
 def create_message(jsondata):
     jsonRes = []
-    for game in jsondata["data"]:
-        minute = game["minute"]
-        home = game["home_team"]["name"]
-        away = game["away_team"]["name"]
-        hscore = game["stats"]["home_score"]
-        ascore = game["stats"]["away_score"]
-        #print(home, hscore, " - ", ascore, away)
-        jsonTupla = {"home": home, "hscore": hscore, "away": away, "ascore": ascore, "minute": minute}
-        jsonRes.append(jsonTupla)
+    if not jsondata["data"]:
+        return json.dumps(None)
+    else:
+        for game in jsondata["data"]:
+            minute = game["minute"]
+            home = game["home_team"]["name"]
+            away = game["away_team"]["name"]
+            hscore = game["stats"]["home_score"]
+            ascore = game["stats"]["away_score"]
+            jsonTupla = {"home": home, "hscore": hscore, "away": away, "ascore": ascore, "minute": minute}
+            jsonRes.append(jsonTupla)
     return json.dumps(jsonRes)
 
 while True:
@@ -42,15 +44,13 @@ while True:
     print(response.text)
     
     jsondata = json.loads(response.text)
-    if not jsondata["data"]:
-        print("SONO DENTRO")
-        message = "null"
-        channel.basic_publish(exchange='livescore', routing_key='livescore_queue', body=message)
-        break
     message = create_message(jsondata)
-    print("\nSTAMPA MESSAGGIO\n", message)
+    if message is None: 
+        break
     channel.basic_publish(exchange='livescore', routing_key='livescore_queue', body=message)
 
 connection.close()
 
-print("Exit from function!!!!")
+print("Exit from function.")
+
+
